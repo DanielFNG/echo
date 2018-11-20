e3807e8b3cc1d0affbb8a5f8fe2aaf3231f1d966
Daniel Gordon <daniel.gordon@ed.ac.uk>
2018-11-20 15:52:53 +0000

    data

--- Source/policy_controller.m
+++ Source/policy_controller.m
@@ -1,9 +1,9 @@
 n_iterations = 20;
 acquisition_function = 'probability-of-improvement';
 
-rise = optimizableVariable('rise', [20, 50]);
-peak = optimizableVariable('peak', [25, 60]);
-fall = optimizableVariable('fall', [50, 70]);
+rise = optimizableVariable('rise', [30, 50]);
+peak = optimizableVariable('peak', [35, 70]);
+fall = optimizableVariable('fall', [40, 90]);
 
 optimisation_variables = [rise, peak, fall];
 
@@ -11,5 +11,6 @@ objective_function = @evaluateCoP_ML_disp;
 
 results = bayesopt(objective_function, ...
     optimisation_variables, ...
+    'XConstraintFcn', @xconstraint, ...
     'AcquisitionFunctionName', acquisition_function, ...
     'MaxObjectiveEvaluations', n_iterations);
