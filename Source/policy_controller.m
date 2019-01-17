2291bb3b2072a03d89225d6f96bf35c9905e29b6
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-01-17 17:26:43 +0000

    Robustness changes
    More incoming, more structural changes to codebase required

--- Source/policy_controller.m
+++ Source/policy_controller.m
@@ -2,7 +2,8 @@ save_directory = 'D:\Dropbox\PhD\HIL Control\Test-13-12-18';
 markers_saveloc = '';
 grfs_saveloc = '';
 
-total_iterations = 20;
+iteration_size = 1;
+max_iterations = 15;
 
 acquisition_function = 'probability-of-improvement';
 
@@ -10,11 +11,29 @@ rise = optimizableVariable('rise', [30, 50], 'Type', 'integer');
 peak = optimizableVariable('peak', [35, 70], 'Type', 'integer');
 fall = optimizableVariable('fall', [40, 90], 'Type', 'integer');
 
+optimisation_variables = [rise, peak, fall];
+
 %objective_function = @evaluateCoP_ML_disp;
 objective_function = @evaluateHipROM;
 
 results = bayesopt(objective_function, ...
     optimisation_variables, ...
-    'XConstraintFcn', @xconstraint, ...
+    'XConstraintFcn', @parameterConstraints, ...
     'AcquisitionFunctionName', acquisition_function, ...
-    'MaxObjectiveEvaluations', total_iterations);
+    'MaxObjectiveEvaluations', iteration_size);
+
+save('intermediate_results.mat', 'results', 'count');
+
+count = 1;
+while count < max_iterations 
+    try
+        old_results = results;
+        results = old_results.resume('MaxObjectiveEvaluations', 1);
+        count = count + 1;
+    catch err
+        disp(err.message);
+        input('Press enter when ready to retry.\n');
+        results = old_results;
+    end
+    save('intermediate_results.mat', 'results', 'count');
+end
