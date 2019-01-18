d475816a2d4b0ae22416e50fdd2b5783f6ae0e19
Daniel Gordon <DanielFNG@users.noreply.github.com>
2019-01-18 14:25:57 +0000

    Updates
    Tidying up & some changes towards increased/full autonomy.

--- Source/parameterConstraints.m
+++ Source/parameterConstraints.m
@@ -1,9 +1,10 @@
 function pass = parameterConstraints(X)
+% Constraints on Bayesian Optimisation [rise, peak, fall] parameters. 
 
 % Assert that rise <= peak <= fall.
 ordered_parameters = (X.rise <= X.peak) & (X.peak <= X.fall);
 
-% Assert minimum length of applied assistance.
+% Assert minimum length of applied assistance - 10% of gait cycle.
 minimum_length = (X.fall - X.rise) > 10;
 
 % Combine constraints. 
--- Source/policy_controller.m
+++ Source/policy_controller.m
@@ -1,39 +1,59 @@
-save_directory = 'D:\Dropbox\PhD\HIL Control\Test-13-12-18';
-markers_saveloc = '';
-grfs_saveloc = '';
+% Save file name.
+save_file = 'intermediate_results.mat';
 
-iteration_size = 1;
+% Control parameterisation settings. 
+peak_force = 10;
+rise_range = [30, 50];
+peak_range = [35, 70];
+fall_range = [40, 90];
+
+% Bayesian optimisation settings. 
 max_iterations = 15;
-
 acquisition_function = 'probability-of-improvement';
-
-rise = optimizableVariable('rise', [30, 50], 'Type', 'integer');
-peak = optimizableVariable('peak', [35, 70], 'Type', 'integer');
-fall = optimizableVariable('fall', [40, 90], 'Type', 'integer');
-
-optimisation_variables = [rise, peak, fall];
-
-%objective_function = @evaluateCoP_ML_disp;
 objective_function = @evaluateHipROM;
+parameter_constraints = @parameterConstraints;
 
+% Subject-specific settings.
+model_file = 'chris_scaled.osim';
+baseline = 49.7;  % Only relevant for fixed baseline objective functions.
+
+% Optimisation variable construction - to be optimised.
+rise = optimizableVariable('rise', rise_range, 'Type', 'integer');
+peak = optimizableVariable('peak', peak_range, 'Type', 'integer');
+fall = optimizableVariable('fall', fall_range, 'Type', 'integer');
+
+% Optimisation variable construction - fixed.
+force = optimizableVariable(...
+    'force', [peak_force, peak_force], 'Optimize', false);
+model = optimizableVariable(...
+    'model', {model_file}, 'Type', 'categorical', 'Optimize', false);
+baseline = optimizableVariable(...
+    'baseline', [baseline, baseline], 'Optimize', false);
+
+% Optimisation variable construction - grouping.
+optimisation_variables = [rise, peak, fall, model];
+
+% Initialise the Bayesian optimisation with a single step. 
+iteration = 1;
 results = bayesopt(objective_function, ...
     optimisation_variables, ...
-    'XConstraintFcn', @parameterConstraints, ...
+    'XConstraintFcn', parameter_constraints, ...
     'AcquisitionFunctionName', acquisition_function, ...
-    'MaxObjectiveEvaluations', iteration_size);
+    'MaxObjectiveEvaluations', 1);
 
-save('intermediate_results.mat', 'results', 'count');
+% Save the result of the initial BO step. 
+save(save_file, 'results', iteration);
 
-count = 1;
-while count < max_iterations 
+% Resume the Bayesian optimisation.
+while iteration < max_iterations 
     try
         old_results = results;
         results = old_results.resume('MaxObjectiveEvaluations', 1);
-        count = count + 1;
+        iteration = iteration + 1;
     catch err
         disp(err.message);
         input('Press enter when ready to retry.\n');
         results = old_results;
     end
-    save('intermediate_results.mat', 'results', 'count');
+    save('intermediate_results.mat', 'results', 'iteration');
 end
