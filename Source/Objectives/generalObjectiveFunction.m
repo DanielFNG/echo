8b9443512c8ec0899ae62d46f00cf6b2153c337c
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-01-23 00:20:14 +0000

    Policy controller + obj function implementation

--- Source/Objectives/generalObjectiveFunction.m
+++ Source/Objectives/generalObjectiveFunction.m
@@ -1,16 +1,37 @@
 function result = generalObjectiveFunction(X)
 
 % Collect global variables
-global metric peak_force model_file baseline iteration;
+global metric args model_file baseline iteration;
+global time_delay marker_rotations grf_rotations;
 global base_dir v_name d_name v_format d_format;
 
+%% Create first-level results directories. 
+persistent processed_dir segment_dir opensim_dir;
+
+if iteration == 1
+    processed_dir = [base_dir filesep 'processed'];
+    segment_dir = [base_dir filesep 'gait_cycles'];
+    opensim_dir = [base_dir filesep 'opensim'];
+    mkdir(processed_dir);
+    mkdir(segment_dir);
+    mkdir(opensim_dir);
+end
+
 % Communicate with APO to apply correct torque pattern.
+% Actually in the mean-time this will be operator controlled, since one
+% will be needed for putting Vicon live anyway. A beep will tell the
+% operator to update the control parameters on the APO side during the
+% non-assisted phase of the current trial.
+fprintf('Apply rise %i, peak %i, fall %i.\n', X.rise, X.peak, X.fall);
+beep;
 
 % Every 2s check for the existence of the trial data.
+marker_name = [v_name sprintf(v_format, iteration)];
+grf_name = [d_name sprintf(d_format, iteration)];
 marker_file = ...
-    [base_dir filesep v_name sprintf(v_format, iteration) '.trc'];
+    [base_dir filesep marker_name '.trc'];
 grf_file = ...
-    [base_dir filesep d_name sprintf(d_format, iteration) '.txt'];
+    [base_dir filesep grf_name '.txt'];
 while true 
     if exist(grf_file, 'file') && exist(marker_file, 'file')
         pause(2);  % Give an additional 2s just for finishing file writing.
@@ -19,14 +40,73 @@ while true
         pause(2);
     end
 end
+% NEED ANOTHER CHECK THAT THESE ARE WRITABLE!
 
-% Construct the OpenSimTrial.
+%% Process the files.
+
+% Assign names.
+processed_markers = [processed_dir filesep marker_name '.trc'];
+processed_grfs = [processed_dir filesep grf_name '.mot'];
+
+% Processing
+int_grf = produceMOT(grf_file, base_dir);
+[markers, grfs] = synchronise(marker_file, int_grf, time_delay);
+markers.rotate(marker_rotations{:});
+grfs.rotate(grf_rotations{:});
+
+% File creation
+markers.writeToFile(processed_markers);
+grfs.writeToFile(processed_grfs);
+
+%% Segmentation
+
+int_seg_dir = [segment_dir filesep 'iteration' sprintf('%03i', iteration)];
+mkdir(int_seg_dir);
+
+% Segment.
+%segment('left', 'stance', 40, processed_grfs, processed_markers, int_seg_dir);
+segment('right', 'stance', 40, processed_grfs, processed_markers, int_seg_dir);
+% Only doing this for right foot for now, for quicker testing.
+
+%% OpenSim compuations
+
+% Obtain the markers & grf files. 
+marker_files = dir([int_seg_dir filesep '*.trc']);
+grf_files = dir([int_seg_dir filesep '*.mot']);
+
+% Create directory for results storage.
+osim_save_dir = [opensim_dir filesep 'iteration' sprintf('%03i', iteration)];
+mkdir(osim_save_dir);
 
 % Run appropriate OpenSim analyses.
+n_cycles = length(marker_files);
+hip_rom = zeros(1, n_cycles);
+for i=1:n_cycles
+    
+    % Create directory for results storage. 
+    results = [osim_save_dir filesep 'cycle' sprintf('%03i', i)];
+    
+    % Create OpenSimTrial
+    ost = OpenSimTrial(...
+        model_file, [int_seg_dir filesep marker_files(i).name], ...
+        results, [int_seg_dir filesep grf_files(i).name]);
+    
+    % Run IK. 
+    ost.run('IK');
+    % ost{i}.run('BK');
+    % ost{i}.run('ID');
+    % Only doing IK for now for ease of testing. 
+    
+    % Compute metric.
+    ost_results = OpenSimResults(ost, {'IK'});
+    hip_rom(i) = metric(ost_results, args{:});
+end
 
 % If required, calculate the relative baseline for this trial. 
+% Not for now.
 
-% Use OpenSimResults to compute & report metric data. 
+% Compute & report metric data. 
+result = sum((hip_rom - baseline).^2)/n_cycles;
 
 end
 
--- Source/policy_controller.m
+++ Source/policy_controller.m
@@ -6,25 +6,31 @@ rise_range = [30, 50];
 peak_range = [35, 70];
 fall_range = [40, 90];
 
-% Global variables - subject/experiment specific.
-global metric peak_force model_file baseline iteration;
-metric = 'hip_rom';
-peak_force = 10;
-model_file = 'chris_scaled.osim';
+% Global variables - subject specific.
+global metric args model_file baseline iteration;
+metric = @calculateROM;
+args = {'hip_flexion_r'};
+model_file = 'C:\Users\danie\Documents\GitHub\echo\Source\chris_scaled.osim';
 baseline = 49.7;  % Real for fixed, 'measured' for measured
+
+% Global variables - experiment settings.
+global time_delay marker_rotations grf_rotations;
+time_delay = 16*(1/600);
+marker_rotations = {0,270,0};  
+grf_rotations = {0,90,0};
 
 % Global variables - filestructure.
 global base_dir v_name d_name v_format d_format;
-base_dir = 'N:\Shared_Data\HIL\Protocol_Setup\21-01-19';
-v_name = 'Markers';
-d_name = 'GRFs';
-v_format = '%04i';  % # of leading 0's in Vicon (trc) filenames 
+base_dir = 'F:\Dropbox\PhD\HIL Control\Automation-test\walking';
+v_name = 'markers';
+d_name = 'NE';
+v_format = '%02i';  % # of leading 0's in Vicon (trc) filenames 
 d_format = '%04i';  % # of leading 0's in D-Flow (txt) filenames
 
 % Bayesian optimisation settings. 
-max_iterations = 15;
+max_iterations = 3;
 acquisition_function = 'probability-of-improvement';
-objective_function = @evaluateHipROM;
+objective_function = @generalObjectiveFunction;
 parameter_constraints = @parameterConstraints;
 
 % Optimisation variable construction.
@@ -33,23 +39,24 @@ peak = optimizableVariable('peak', peak_range, 'Type', 'integer');
 fall = optimizableVariable('fall', fall_range, 'Type', 'integer');
 optimisation_variables = [rise, peak, fall];
 
-% Initialise the Bayesian optimisation with a single step. 
+% Initialise the Bayesian optimisation with a single step.
 iteration = 1;
 results = bayesopt(objective_function, ...
     optimisation_variables, ...
     'XConstraintFcn', parameter_constraints, ...
     'AcquisitionFunctionName', acquisition_function, ...
-    'MaxObjectiveEvaluations', 1);
+    'MaxObjectiveEvaluations', 1, ...
+    'PlotFcn', []);
 
 % Save the result of the initial BO step. 
-save(save_file, 'results', iteration);
+save(save_file, 'results', 'iteration');
 
 % Resume the Bayesian optimisation.
-while iteration < max_iterations 
+while iteration <= max_iterations - 1
     try
+        iteration = iteration + 1;
         old_results = results;
         results = old_results.resume('MaxObjectiveEvaluations', 1);
-        iteration = iteration + 1;
     catch err
         disp(err.message);
         input('Press enter when ready to retry.\n');
