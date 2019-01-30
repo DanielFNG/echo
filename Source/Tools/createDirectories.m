75fd6ea72e2b55c3dc223d6d735445da8d3e8908
Daniel Gordon <DanielFNG@users.noreply.github.com>
2019-01-30 14:44:54 +0100

    Move tools to separate repo

--- Source/Tools/createDirectories.m
+++
@@ -1,9 +0,0 @@
-function createDirectories(dir_struct)
-% Create directories given a stucture holding directory paths.
-
-dirs = fieldnames(dir_struct);
-for i=1:length(dirs)
-    mkdir(dirs(i));
-end
-
-end
