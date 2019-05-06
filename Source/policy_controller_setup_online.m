532542d48d8f43ecb61c8095ea287a999a2e7a08
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-05-06 21:07:36 +0100

    Bit tidy up and more data files

--- Source/policy_controller_setup_online.m
+++ Source/policy_controller_setup_online.m
@@ -1,20 +1,29 @@
-% Setup script for policy controller implementation. This script should be 
-% modified to suit experiment/subject/control settings & used as the base 
-% script to run the policy controller. 
+%% The stuff that probably has to change/be looked over between experiments.
 
 % Save file name - where the bayesopt results will be saved.
-settings.save_file = 'wut.mat';
+settings.save_file = 'calum_peak_hip.mat';
+
+% Data inputs - markers only, grfs only, or both (motion).
+settings.data_inputs = 'Motion';
+
+% Subject specific settings.
+settings.model_file = 'C:\Users\danie\Documents\GitHub\echo\Source\calum_scaled.osim';
+settings.leg_length = 0.91;
+settings.toe_length = 0.085;
+
+% Metric specific settings.
+settings.metric = @calculatePeak;
+settings.args = {'hip_flexion_r'};
+settings.opensim_analyses = {'IK'};
+settings.motion_analyses = {'GRF', 'IK'};
+
+% Data directory.
+settings.base_dir = 'D:\Share\Shared_Data\HIL\Data Collection With Kinematics\Calum Peak Hip';
+
+%% The stuff that probably doesn't have to change/be looked over.
 
 % Operation mode - online or offline.
 settings.operation_mode = 'online';
-
-% Data inputs - markers only, grfs only, or both (motion).
-settings.data_inputs = 'GRF';
-
-% Subject specific settings.
-settings.model_file = 'C:\Users\danie\Documents\GitHub\echo\Source\chris_scaled.osim';
-settings.leg_length = 0.93;
-settings.toe_length = 0.08;
 
 % Experiment specific settings.
 settings.speed = 0.8;
@@ -25,7 +34,7 @@ settings.feet = {'right'};
 settings.segmentation_mode = 'stance';
 settings.segmentation_cutoff = 2;
 settings.time_delay = 16*(1/600);  
-settings.marker_rotations = {0,270,0};  
+settings.marker_rotations = {0,90,0};  
 settings.grf_rotations = {0,90,0};
 
 % Valid ranges for the control parameters. NOTE: if
@@ -42,20 +51,11 @@ settings.min_length = 10;
 % Baseline mode - none, absolute or relative.
 settings.baseline_mode = 'absolute';
 settings.baseline_filename = 'baseline';
-%settings.baseline = 35.2809;  % HipRoM with APO.
-%settings.baseline = 29.3065;  % HipRoM without APO.
 
 % Communication - this should be an active TCPIP server. 
 settings.server = t;
 
-% Metric specific settings.
-settings.metric = @calculateTotalTime;
-settings.args = {};
-settings.opensim_analyses = {};
-settings.motion_analyses = {'GRF'};
-
-% Filestructure.
-settings.base_dir = 'D:\Share\Shared_Data\HIL\Data Collection\Cadence';
+% Data filestructure.
 settings.v_name = 'markers';
 settings.d_name = 'grf';
 settings.v_format = '%02i';  % # of leading 0's in Vicon (trc) filenames 
@@ -64,10 +64,7 @@ settings.d_format = '%04i';  % # of leading 0's in D-Flow (txt) filenames
 % Bayesian optimisation settings. 
 settings.max_iterations = 20;
 settings.acquisition_function = 'expected-improvement';
-
-% Additional arguments to the bayesopt function. 
-settings.bayesopt_args = {};%{'ExplorationRatio', 0.8};
-
+settings.bayesopt_args = {};  % stuff like exploration ratio would be here
 
 %% Run
 
