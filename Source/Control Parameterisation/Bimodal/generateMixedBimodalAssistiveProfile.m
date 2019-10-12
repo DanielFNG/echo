6987615aca3ed8e57e52326aa86c4ceb18229b87
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-10-12 16:54:33 +0100

    Fixes to mixed profile generation

--- Source/Control Parameterisation/Bimodal/generateMixedBimodalAssistiveProfile.m
+++ Source/Control Parameterisation/Bimodal/generateMixedBimodalAssistiveProfile.m
@@ -1,5 +1,5 @@
 function y = generateMixedBimodalAssistiveProfile(...
-    n_points, force, rise, pflex, fall, pext)
+    n_points, force, pext, rise, pflex, fall)
 % Generate a parameterised assistive torque profile.
 %   Similar to generateAssistiveProfile, however this function mixes 
 %   portions of sinusoidal waves which are centred at different levels
@@ -8,10 +8,10 @@ function y = generateMixedBimodalAssistiveProfile(...
 % --- Inputs:
 % ---------- n_points = # of frames comprising trajectory 
 % ---------- force = peak extension torque attained 
+% ---------- pext = % at which peak extension assistance occurs
 % ---------- rise = % crossing point during rising phase
 % ---------- pflex = % at which peak flexion assistance occurs
 % ---------- fall = % crossing point during falling phase
-% ---------- pext = % at which peak extension assistance occurs
 % ---------- 
 % --- Output:
 % ---------- y = trajectory as an array of length n_points
@@ -25,78 +25,45 @@ function y = generateMixedBimodalAssistiveProfile(...
     y = zeros(n_points, 1);
     x = linspace(0.0, 100.0, n_points);
     
-    % Initialise some parameters.
-    rise_point = round((rise/101)*n_points);
-    fall_point = round((fall/101)*n_points);
-    pflex_point = round((pflex/101)*n_points);
-    pext_point = round((pext/101)*n_points);
+    % An alternative initialisation. 
+    rise_point = floor((rise/100)*n_points) + 1;
+    fall_point = floor((fall/100)*n_points) + 1;
+    pflex_point = floor((pflex/100)*n_points) + 1;
+    pext_point = floor((pext/100)*n_points) + 1;
+%   % Sanity check that the initialisation above is working - it is.
+%     [~, rise_point] = min(abs(x - rise));
+%     [~, fall_point] = min(abs(x - fall));
+%     [~, pflex_point] = min(abs(x - pflex));
+%     [~, pext_point] = min(abs(x - pext));
 
     % Check that we're in a valid ordering.
-    if issorted([rise pflex fall pext])
-        scenario = 1;
-    elseif issorted([pext rise pflex fall])
-        scenario = 2;
-    elseif issorted([fall pext rise pflex]) 
-        scenario = 3;
-    else
+    if ~issorted([pext rise pflex fall])
         error('Shape parameters are invalid.');
     end
+
+    % Set the extension falling region
+    lambda = pi/(rise - pext);
+    y(pext_point:rise_point - 1) = (f_ext/2)* ...
+        sin(lambda*x(1:rise_point - pext_point) - pi/2) - f_ext/2;
     
-    % Set the flexion rising region - common to all methods
+    % Set the flexion rising region 
     lambda = pi/(pflex - rise);
     y(rise_point:pflex_point - 1) = (f_flex/2)* ...
         sin(lambda*x(1:pflex_point - rise_point) - pi/2) + f_flex/2;
     
-    switch scenario
-        case 1
-            % Set the flexion falling region.
-            lambda = pi/(fall - pflex);
-            y(pflex_point:fall_point - 1) = (f_flex/2)* ...
-                sin(lambda*x(1:fall_point - pflex_point) + pi/2) + f_flex/2;
-            
-            % Set the extension rising region.
-            lambda = pi/(pext - fall);
-            y(fall_point:pext_point - 1) = (f_ext/2)* ...
-                sin(lambda*x(1:pext_point - fall_point) + pi/2) - f_ext/2;
-            
-            % Set the end and start - both portions of extension falling
-            lambda = pi/(rise - pext);
-            n = length(y(pext_point:end));
-            m = length(y(1:rise_point - 1));
-            y(pext_point:end) = (f_ext/2)* ...
-                sin(lambda*x(1:n) + pi/2) - f_ext/2;
-            y(1:m) = (f_flex/2)* ...
-                sin(lambda*x(n + 1:rise_point - pext_point) + pi/2) - f_flex/2;
-    end
-
-    % Repeat more or less the same steps twice
-    for i = 1:2
-
-        
-        
-        % Set the flexion falling region.
-        lambda = pi/(fall - pflex);
-        y(pflex_point:fall_point - 1) = (f_flex/2)* ...
-            sin(lambda*x(1:fall_point - pflex_point) + pi/2) + f_flex/2;
-
-        % Set the extension rising region.
-        lambda = pi/(pext - fall);
-        y(fall_point:pext_point - 1) = (f_ext/2)* ...
-            sin(lambda*x(1:pext_point - fall_point) + pi/2) - f_ext/2;
-        
-        % Scale the parameters.
-        rise_point = rise_point + i*n_points;
-        fall_point = fall_point + i*n_points;
-        
-        % Set the extension falling region.
-        lambda = pi/(rise + 100 - pext);
-        y(pext_point:rise_point - 1) = (f_ext/2)* ...
-            sin(lambda*x(1:rise_point - pext_point) - pi/2) - f_ext/2;
-        
-        % Scale more parameters.
-        pflex_point = pflex_point + i*n_points;
-        pext_point = pext_point + i*n_points;
-
-    end
+    % Set the flexion falling region.
+    lambda = pi/(fall - pflex);
+    y(pflex_point:fall_point - 1) = (f_flex/2)* ...
+        sin(lambda*x(1:fall_point - pflex_point) + pi/2) + f_flex/2;
+    
+    % Set the extension rising region.
+    lambda = pi/(100 + pext - fall);
+    end_point = n_points - fall_point + 1;
+    start_point = pext_point - 1;
+    y(fall_point:end) = (f_ext/2)* ...
+        sin(lambda*x(1:end_point) + pi/2) - f_ext/2;
+    y(1:pext_point - 1) = (f_ext/2)* ...
+        sin(lambda*x(end_point + 1: end_point + start_point) + pi/2) - ...
+        f_ext/2;
 
 end
